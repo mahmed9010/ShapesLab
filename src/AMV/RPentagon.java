c48e7ed5ea07217b1c92ff7d245bd4f1f3f9fd8d
zfeng7527 <zfeng7527@bths.edu>
2018-10-04 11:05:34 -0400

    RPentagon added

---
+++ src/AMV/RPentagon.java
@@ -0,0 +1,22 @@
+package AMV;
+
+public class RPentagon extends Shape {
+    private int side;
+
+    public RPentagon(int side)
+    {
+        super(type: "RPentagon");
+        this.side = side;
+    }
+    @Override
+    public double getArea()
+    {
+        return 0.25*Math.sqrt(5*(5+2*Math.sqrt(5)))*(side*side);
+    }
+
+    @Override
+    public double getPerimeter()
+    {
+        return side*5;
+    }
+}
